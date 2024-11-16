82572eb145b3b45d0a1d5887f6d233ad9514c302
David Urdaneta <urdanetarhcp@gmail.com>
2024-11-15 22:32:43 -0500

    add: add solution for hack_2.py

---
+++ hack_2.py
@@ -0,0 +1,11 @@
+def fn_hack_2(s):
+    result = s
+    vowels = ["a","e","i","o","u"]
+    _str = []
+    
+    for txt in result:
+        if txt not in vowels:
+          _str.append(txt)  
+          
+    result = "".join(_str)
+    return result
